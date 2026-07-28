44f73b07d8b5e3cea263248201c118c61b50b545
MrNorwood <[email protected]>
2026-07-28 16:07:21 +0200

    remove nulled slop

--- kubejs/server_scripts/worldgen/ore_veins.js
+++ kubejs/server_scripts/worldgen/ore_veins.js
@@ -1,26 +1,3 @@
-GTCEuServerEvents.oreVeins(event => {
-    event.add('kubejs:clay_pipe_material_vein', vein => {
-        vein.weight(100)
-        vein.clusterSize(30)
-        vein.density(0.2)
-        vein.discardChanceOnAirExposure(0)
-        vein.layer('stone')
-        vein.dimensions('minecraft:overworld')
-        vein.biomes('minecraft:swamp', 'minecraft:river')
-        vein.heightRangeUniform(-32, 64)
-        vein.dikeVeinGenerator(generator => generator
-        .withBlock(GTMaterials.get('clay_pipe_material'), 1, 0, 100)
-        )
-        vein.surfaceIndicatorGenerator(indicator => indicator
-        .block(Block.getBlock('gtceu:clay_pipe_material_ore'))
-        .placement('above')
-        .density(0.4)
-        .radius(5)
-        )
-    })
-})
-
-
 // --- Relocate GregTech Nether ore veins into the Overworld (was nether_to_overworld_ores.js) ---
 // Move all GregTech Nether ore veins into the Overworld, so the Nether is not
 // required for progression, and spread them vertically:
